4c318f9a2f7943c101ad5756e0eafefa327e7ebf
Sokolova Evgeniya <evsokol91@mail.ru>
2015-10-01 12:53:32 +0300

    Add onchange handler for the list

--- plugins/freeboard/dronlist.widget.js
+++ plugins/freeboard/dronlist.widget.js
@@ -2,16 +2,25 @@
     var DroneListPlugin = function(settings) {
         var self = this;
 
-        var listContainer = $('<select style="width:100%; heihgt:100%; margin-top:25px;" onChange="changeFunction(this)"><option value="1">Drone 1</option><option value="2">Drone 2</option><option value="3">Drone 3</option></select>');
+        var $listContainer = $('<select style="width:100%; heihgt:100%; margin-top:25px;"><option value="1">Drone 1</option><option value="2">Drone 2</option><option value="3">Drone 3</option></select>');
 
         self.render = function(containerElement) {
             console.log('render');
-            $(containerElement).append(listContainer);
+            $(containerElement).append($listContainer);
         };
 
         self.getHeight = function() {
             return 2;
         };
+
+        var state;
+
+        $listContainer.on('change', function () {
+            if (typeof state == 'function') {
+                state($listContainer.val());
+            }
+            console.log("selected value was changed on " + $listContainer.val());
+        });
 
         self.onSettingsChanged = function(newSettings) {
             // just update the original ones
@@ -19,8 +28,11 @@
             settings = newSettings;
         };
 
-        self.onCalculatedValueChanged = function(settingName, newValue) {
-            console.log('onCalculatedValueChanged, ' + settingName + '=' + newValue);
+        self.onCalculatedValueChanged = function (settingName, newValue) {
+            if (settingName === 'state' && typeof newValue == 'function') {
+                state = newValue;
+                $listContainer.val(state());
+            }
         };
 
         self.onDispose = function() {
@@ -34,7 +46,14 @@
         'display_name': 'List of Drones',
         'description': 'This plugin is for selecting of drones',
         'fill_size': false,
-        'settings': {},
+        'settings':[
+        {
+            'name': 'state',
+            'display_name': 'State',
+            'type': 'calculated',
+            'default_value': null,
+            'description': 'State to be changed on selecting new value' 
+        }],
         newInstance: function (settings, newInstanceCallback) {
             newInstanceCallback(new DroneListPlugin(settings));
         }
